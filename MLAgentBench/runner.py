3d9a61d9c5e88d336380f6a54dca258202ae3118
yunx-z <yunxiang@umich.edu>
2025-01-09 18:37:28 -0500

    bug fix

--- MLAgentBench/runner.py
+++ MLAgentBench/runner.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("--task", type=str, default="debug", help="task name")
     parser.add_argument("--log-dir", type=str, default="./logs", help="log dir")
     parser.add_argument("--work-dir", type=str, default="./workspace", help="work dir")
-    parser.add_argument("--max-steps", type=int, default=50, help="number of steps")
+    parser.add_argument("--max-steps", type=int, default=50, help="number of steps in environment, including those with errors")
     parser.add_argument("--max-time", type=int, default=5* 60 * 60, help="max time")
     parser.add_argument("--max-api-cost", type=int, default=10, help="max api cost in dollars")
     parser.add_argument("--device", type=int, default=0, help="device id")
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     parser.add_argument("--feedback-llm-max-tokens", type=int, default=4000, help="llm max tokens")
     parser.add_argument("--edit-script-llm-name", type=str, default="claude-v1", help="llm name")
     parser.add_argument("--edit-script-llm-max-tokens", type=int, default=4000, help="llm max tokens")
-    parser.add_argument("--agent-max-steps", type=int, default=50, help="max iterations for agent")
+    parser.add_argument("--agent-max-steps", type=int, default=50, help="max iterations for agent, only counting valid steps without parsing error")
 
     # research agent configs
     parser.add_argument("--actions-remove-from-prompt", type=str, nargs='+', default=[], help="actions to remove in addition to the default ones: Read File, Write File, Append File, Retrieval from Research Log, Append Summary to Research Log, Python REPL, Edit Script Segment (AI)")
