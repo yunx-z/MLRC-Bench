6955471460d658674cbbab0de968f096018a667a
yunx-z <yunxiang@umich.edu>
2025-01-26 20:53:32 -0500

    update

--- MLAgentBench/benchmarks_base/backdoor-trigger-recovery/scripts/test_constants.py
+++ MLAgentBench/benchmarks_base/backdoor-trigger-recovery/scripts/test_constants.py
@@ -1,4 +1,4 @@
-test_model_id = "Zhaorun/CodeQwen1.5-7B-trojan-clas2024-test"
-test_trigger_data = "test_trigger_gt.json"
-test_target_list = "target_list_testing.json"
-test_prompt_data = "hidden_test.jsonl"
+test_model_id = "Zhaorun/CodeQwen1.5-7B-trojan-clas2024-development"
+test_trigger_data = "trigger_gt.json"
+test_target_list = "target_list.json"
+test_prompt_data = "test.jsonl" # change to "test.jsonl" for public test
