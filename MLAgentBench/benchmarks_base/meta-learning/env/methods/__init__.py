d7f49b4a1eb1bc293d47ce397aa81bd3626d1008
Grant D Murphy <grantdillermurphy@gmail.com>
2025-02-01 14:36:36 -0500

    Removed competition methods from meta learning env

--- MLAgentBench/benchmarks_base/meta-learning/env/methods/__init__.py
+++ MLAgentBench/benchmarks_base/meta-learning/env/methods/__init__.py
@@ -5,8 +5,6 @@ def all_method_handlers():
     loaded_method_dirs = {
             "my_method" : "protonet",
             "random" : "random",
-            "meta_beyond" : "meta_beyond",
-            "Emmanuel_Pintelas" : "Emmanuel_Pintelas"
             }
  
 
