73735562b6ccce46f243d9de04c2a7de9e196ef6
Grant D Murphy <grantdillermurphy@gmail.com>
2025-02-17 02:40:50 -0500

    updated to mp 2.3.0 for feather file

--- MLAgentBench/benchmarks_base/melting-pot/scripts/prepare.py
+++ MLAgentBench/benchmarks_base/melting-pot/scripts/prepare.py
@@ -1,7 +1,7 @@
 # import subprocess
 
 # url = "https://storage.googleapis.com/dm-meltingpot/meltingpot-results-2.3.0.feather"
-# save_path = "../env/meltingpot-results-2.1.1.feather"
+# save_path = "../env/meltingpot-results-2.3.0.feather"
 
 # # Run wget with the -O flag to specify the output location
 # subprocess.run(["wget", url, "-O", save_path], check=True)
