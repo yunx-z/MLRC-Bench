3c1a9f2439702877307fde9acab25ace6eda50df
Xinnuo Li <monmonil@umich.edu>
2025-03-18 16:00:51 -0400

    add warmup

--- MLAgentBench/benchmarks_base/erasing_invisible_watermarks_warmup/warm-up-kit/setup.py
+++ MLAgentBench/benchmarks_base/erasing_invisible_watermarks_warmup/warm-up-kit/setup.py
@@ -8,9 +8,10 @@ setup(
     name="erasinginvisible",
     version="0.1",
     packages=find_packages(),
+    py_modules=["cli"],
     install_requires=requirements,
     entry_points={
-        "console_scripts": ["erasinginvisible=erasinginvisible.cli:cli"]
+        "console_scripts": ["erasinginvisible=cli:cli"]  # Pointing to the cli function in cli.py
     },
     # Other metadata
 )
